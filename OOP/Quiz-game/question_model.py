c6aa5eb0e3c01c8e3b0614d0f5ebc0861caf1598
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-23 20:44:22 +0530

    Quiz game with OOP

---
+++ OOP/Quiz-game/question_model.py
@@ -0,0 +1,5 @@
+# model of the question. get the questions and answers
+class Question:
+    def __init__(self, q_text, q_answer):  # constructor
+        self.text = q_text
+        self.answer = q_answer
