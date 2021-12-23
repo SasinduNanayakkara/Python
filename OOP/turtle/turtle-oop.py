98a8c0d84b444faeb33dfc64b86316b752707261
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-23 20:44:53 +0530

    turtle graphicswith OOP

---
+++ OOP/turtle/turtle-oop.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD:OOP/turtle/turtle.py
+from turtle import Turtle, Screen
+
+=======
+#from turtle import Turtle, Screen
+from turtle import Turtle, Screen
+>>>>>>> f9d460197e238962a268415c32b225c94d485a2c:OOP/turtle/turtle-oop.py
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight())
+my_screen.exitonclick()
