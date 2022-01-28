68168dc774d05a40548bd20814a73ac1090c25e8
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-29 01:23:24 +0530

    snake class and movement methods

---
+++ GUI/Snake-game/snake.py
@@ -0,0 +1,67 @@
+# snake class
+from turtle import Turtle, Screen
+
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+
+class Snake:
+
+    def __init__(self): # constructor
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # create a snake
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segments(position)
+
+    # add one block when snake eat a food
+    def add_segments(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extends(self):
+        # add new segments to the snake
+        self.add_segments(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # move the all the segments like ones
+    def move(self):
+
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def Up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def Down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def Left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def Right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
